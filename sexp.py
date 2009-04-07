df699fbcef0551b57fbcf593691a41515ef3b880
jtolds <jtolds@f2aba904-ff02-11dd-8c4e-4bbb75a408b7>
2009-04-07 23:30:22 +0000

    fixing for loop indentation, changing all keywords to be uppercase (assists with macro writing)

--- sexp.py
+++ sexp.py
@@ -41,7 +41,7 @@ def o(n, i, c, handledattrs=[]):
             if len(props_to_include) > 0:
                 s = " (@"
                 for prop in props_to_include:
-                    s += " (%s %s)" % (prop, getattr(n, prop))
+                    s += " (%s %s)" % (prop.upper(), getattr(n, prop))
                 return s +")"
         return ""
     try:
@@ -52,114 +52,114 @@ def o(n, i, c, handledattrs=[]):
 
         if n.type == "ARRAY_INIT":
             check(subnodes=len(n))
-            s = "(array_init%s" % props()
+            s = "(ARRAY_INIT%s" % props()
             for x in n:
                 if x is not None:
                     s += " " + o(x,i,c)
                 else:
-                    s += " null"
+                    s += " NULL"
             return s + ")"
 
         elif n.type == "ASSIGN":
             check(subnodes=2)
             s = "("
             if getattr(n[0],"assignOp", None) is not None:
-                s += "update%s " % props() + js.tokens[n[0].assignOp]
+                s += "UPDATE%s " % props() + js.tokens[n[0].assignOp]
             else:
-                s += "assign%s" % props()
+                s += "ASSIGN%s" % props()
             return s + " %s %s)" % (o(n[0], i, c, handledattrs=["assignOp"]),
                     o(n[1], i, c))
 
         elif n.type == "BLOCK":
             check(subnodes=len(n))
             if len(n) > 0:
-                return "(block%s\n  " % props() + i + \
+                return ("(BLOCK%s\n  " % props()) + i + \
                         ("\n  "+i).join((o(x,i+"  ",c) for x in n)) + ")"
-            return "(block%s)" % props()
+            return "(BLOCK%s)" % props()
 
         elif n.type in ("BREAK", "CONTINUE"):
             check(attrs=["target"], optattrs=["label"])
             if hasattr(n,"label"):
-                return "(%s%s %s)" % (n.value, props(), n.label)
-            return "(%s%s)" % (n.value, props())
+                return "(%s%s %s)" % (n.value.upper(), props(), n.label)
+            return "(%s%s)" % (n.value.upper(), props())
 
         elif n.type == "CALL":
             check(subnodes=2)
-            return "(call%s %s%s)" % (props(), o(n[0], i, c), o(n[1], i, c))
+            return "(CALL%s %s%s)" % (props(), o(n[0], i, c), o(n[1], i, c))
 
         elif n.type == "CASE":
             check(attrs=["caseLabel","statements"])
-            return "(case%s %s %s)" % (props(), o(n.caseLabel,i,c),
+            return "(CASE%s %s %s)" % (props(), o(n.caseLabel,i,c),
                     o(n.statements,i,c))
 
         elif n.type == "CATCH":
             check(attrs=["block","guard","varName"])
             if n.guard is not None:
-                return "(guarded-catch%s %s %s %s)" % (props(), n.varName,
+                return "(GUARDED-CATCH%s %s %s %s)" % (props(), n.varName,
                         o(n.guard,i,c), o(n.block,i,c))
-            return "(catch%s %s %s)" % (props(), n.varName, o(n.block,i,c))
+            return "(CATCH%s %s %s)" % (props(), n.varName, o(n.block,i,c))
 
         elif n.type == "COMMA":
             check(subnodes=2)
-            return "(comma%s %s %s)" % (props(), o(n[0],i,c), o(n[1],i,c))
+            return "(COMMA%s %s %s)" % (props(), o(n[0],i,c), o(n[1],i,c))
 
         elif n.type == "DEBUGGER":
-            return "(debugger%s)" % props()
+            return "(DEBUGGER%s)" % props()
 
         elif n.type == "DEFAULT":
             check(attrs=["statements"])
-            return "(default%s %s)" % (props(), o(n.statements,i,c))
+            return "(DEFAULT%s %s)" % (props(), o(n.statements,i,c))
 
         elif n.type in ("DELETE", "TYPEOF", "NEW", "UNARY_MINUS", "NOT",
                 "VOID", "BITWISE_NOT", "UNARY_PLUS"):
             check(subnodes=1)
-            return "(%s%s %s)" % (n.value, props(), o(n[0],i,c))
+            return "(%s%s %s)" % (n.value.upper(), props(), o(n[0],i,c))
 
         elif n.type == "DO":
             check(attrs=["body", "condition", "isLoop"])
             assert n.isLoop
-            return "(do-while%s %s %s)" % (props(), o(n.condition,i,c),
+            return "(DO-WHILE%s %s %s)" % (props(), o(n.condition,i,c),
                     o(n.body,i,c))
 
         elif n.type == "DOT":
             check(subnodes=2)
-            return "(attribute%s %s %s)" % (props(), o(n[0],i,c), o(n[1],i,c))
+            return "(ATTRIBUTE%s %s %s)" % (props(), o(n[0],i,c), o(n[1],i,c))
 
         elif n.type == "FUNCTION":
             check(attrs=["functionForm","params","body"],
                     optattrs=["name"])
             if n.functionForm == 0:
-                return "(def-function%s (%s%s) %s)" % (props(), n.name,
+                return "(DEF-FUNCTION%s (%s%s) %s)" % (props(), n.name,
                         "".join(" " + param for param in n.params),
                         o(n.body,i,c))
             else:
-                return "(function%s (%s) %s)" % (props(), " ".join(n.params),
+                return "(FUNCTION%s (%s) %s)" % (props(), " ".join(n.params),
                         o(n.body,i,c))
 
         elif n.type == "FOR":
             check(attrs=["body","setup","condition","update","isLoop"])
             assert n.isLoop
             if n.setup is not None: setup = o(n.setup,i,c)
-            else: setup = "(skip)"
+            else: setup = "(BLOCK)"
             if n.condition is not None: condition = o(n.condition,i,c)
-            else: condition = "(skip)"
+            else: condition = "(BLOCK)"
             if n.update is not None: update = o(n.update,i,c)
-            else: update = "(skip)"
-            if n.body is not None: body = o(n.body,i,c)
-            else: body = "(skip)"
-            return "(for%s %s %s %s\n  %s%s)" % (props(), setup, condition,
+            else: update = "(BLOCK)"
+            if n.body is not None: body = o(n.body,i+"  ",c)
+            else: body = "(BLOCK)"
+            return "(FOR%s %s %s %s\n  %s%s)" % (props(), setup, condition,
                     update, i, body)
 
         elif n.type == "FOR_IN":
             check(attrs=["body","iterator","object","isLoop","varDecl"])
             assert n.isLoop
-            s = "(for-in"
+            s = "(FOR-IN"
             if n.varDecl:
                 assert n.varDecl.type == "VAR"
                 assert len(n.varDecl) == 1
                 assert n.varDecl[0].type == "IDENTIFIER"
                 assert n.varDecl[0].value == n.iterator.value
-                s += "-var"
+                s += "-VAR"
             return s + "%s %s %s %s)" % (props(), o(n.iterator,i,c),
                     o(n.object,i,c), o(n.body,i,c))
 
@@ -169,7 +169,7 @@ def o(n, i, c, handledattrs=[]):
 
         elif n.type == "HOOK":
             check(subnodes=3)
-            return "(ternary%s %s %s %s)" % (props(),o(n[0],i,c),o(n[1],i,c),
+            return "(TERNARY%s %s %s %s)" % (props(),o(n[0],i,c),o(n[1],i,c),
                     o(n[2],i,c))
 
         elif n.type == "IDENTIFIER":
@@ -177,17 +177,17 @@ def o(n, i, c, handledattrs=[]):
 #            if getattr(n,"readOnly",False): assert hasattr(n,"initializer")
             if hasattr(n,"name"): assert n.name == n.value
             if hasattr(n,"initializer"):
-                return "(initvar%s %s %s)" % (props(), n.value,
+                return "(INITVAR%s %s %s)" % (props(), n.value,
                         o(n.initializer, i, c))
             return str(n.value)
 
         elif n.type == "IF":
             check(attrs=["condition","thenPart","elsePart"])
             if n.elsePart:
-                return "(if-else%s %s\n  %s%s\n  %s%s)" % (props(),
+                return "(IF-ELSE%s %s\n  %s%s\n  %s%s)" % (props(),
                         o(n.condition,i,c), i, o(n.thenPart,i+"  ",c), i,
                         o(n.elsePart, i+"  ", c))
-            return "(if_%s %s\n  %s%s)" % (props(), o(n.condition,i,c), i,
+            return "(IF%s %s\n  %s%s)" % (props(), o(n.condition,i,c), i,
                     o(n.thenPart, i+"  ",c))
 
         elif n.type in ("INCREMENT", "DECREMENT"):
@@ -198,11 +198,11 @@ def o(n, i, c, handledattrs=[]):
 
         elif n.type == "INDEX":
             check(subnodes=2)
-            return "(array-index%s %s %s)" % (props(), o(n[0],i,c), o(n[1],i,c))
+            return "(ARRAY-INDEX%s %s %s)" % (props(), o(n[0],i,c), o(n[1],i,c))
 
         elif n.type == "LABEL":
             check(attrs=["label","statement"])
-            return "(labeled-stmt%s %s\n  %s%s)" % (props(), n.label, i,
+            return "(LABELED-STATEMENT%s %s\n  %s%s)" % (props(), n.label, i,
                     o(n.statement, i+"  ", c))
 
         elif n.type == "LIST":
@@ -210,44 +210,47 @@ def o(n, i, c, handledattrs=[]):
             return ''.join((' ' + o(x, i, c) for x in n))
 
         elif n.type in ("NUMBER", "TRUE", "FALSE", "THIS", "NULL"):
-            return str(n.value)
+            return str(n.value).upper()
 
         elif n.type == "OBJECT_INIT":
             check(subnodes=len(n))
             if len(n) > 0:
-                return ("(object_init%s\n  " % props() + i +
+                return ("(OBJECT_INIT%s\n  " % props() + i +
                         ("\n  "+i).join(o(x,i+"  ",c) for x in n) + ")")
-            return "(object_init%s)" % props()
+            return "(OBJECT_INIT%s)" % props()
 
         elif n.type in ("PLUS", "LT", "EQ", "AND", "OR", "MINUS", "MUL", "LE",
                 "NE", "STRICT_EQ", "DIV", "GE", "INSTANCEOF", "IN", "GT",
                 "NEW_WITH_ARGS", "BITWISE_OR", "BITWISE_AND", "BITWISE_XOR",
                 "STRICT_NE", "LSH", "RSH", "URSH", "MOD"):
             check(subnodes=2)
-            return "(%s%s %s %s)" % (n.value, props(), o(n[0],i,c), o(n[1],i,c))
+            return "(%s%s %s %s)" % (n.value.upper(), props(), o(n[0],i,c),
+                    o(n[1],i,c))
 
         elif n.type == "PROPERTY_INIT":
             check(subnodes=2)
-            return "(property%s %s %s)" % (props(), o(n[0],i,c), o(n[1],i,c))
+            return "(PROPERTY%s %s %s)" % (props(), o(n[0],i,c), o(n[1],i,c))
 
         elif n.type == "REGEXP":
-            return "(regexp%s %r %r)" % (props(), n.value["regexp"],
+            return "(REGEXP%s %r %r)" % (props(), n.value["regexp"],
                     n.value["modifiers"])
 
         elif n.type == "RETURN":
             if type(n.value) == str:
-                return "(return%s %r)" % (props(), n.value)
-            return "(return%s %s)" % (props(), o(n.value, i, c))
+                return "(RETURN%s %r)" % (props(), n.value)
+            return "(RETURN%s %s)" % (props(), o(n.value, i, c))
 
         elif n.type == "SCRIPT":
             check(attrs=["funDecls","varDecls"], subnodes=len(n))
 #            sys.stderr.write("WARNING: skipping funDecls and varDecls\n")
-            return ("(script%s\n  " % props() + i +
-                    ("\n  "+i).join((o(x,i+"  ",c) for x in n)) + ")")
+            if len(n) > 0:
+                return ("(SCRIPT%s\n  " % props() + i +
+                        ("\n  "+i).join((o(x,i+"  ",c) for x in n)) + ")")
+            return "(SCRIPT%s)" % props()
 
         elif n.type == "SEMICOLON":
             check(attrs=["expression"])
-            if not n.expression: return "(skip)"
+            if not n.expression: return "(BLOCK)"
             return o(n.expression, i, c)
 
         elif n.type == "STRING":
@@ -257,39 +260,39 @@ def o(n, i, c, handledattrs=[]):
             check(attrs=["cases", "defaultIndex", "discriminant"])
             assert (n.defaultIndex == -1 or
                     n.cases[n.defaultIndex].type == "DEFAULT")
-            return "(switch%s %s\n  %s%s)" % (props(), o(n.discriminant,i,c), i,
+            return "(SWITCH%s %s\n  %s%s)" % (props(), o(n.discriminant,i,c), i,
                     ("\n  "+i).join(o(x,i+"  ",c) for x in n.cases))
 
         elif n.type == "THROW":
             check(attrs=["exception"])
-            return "(throw%s %s)" % (props(), o(n.exception,i,c))
+            return "(THROW%s %s)" % (props(), o(n.exception,i,c))
 
         elif n.type == "TRY":
             check(attrs=["catchClauses","tryBlock"], optattrs=["finallyBlock"])
             if hasattr(n,"finallyBlock"):
-                return "(try-finally%s\n  " % props() + i + ("\n  "+i).join(
+                return "(TRY-FINALLY%s\n  " % props() + i + ("\n  "+i).join(
                         [o(n.tryBlock,i+"  ",c)] + [o(x,i+"  ",c)
                         for x in n.catchClauses] + \
                         [o(n.finallyBlock,i+"  ",c)]) + ")"
-            return "(try%s\n  " % props() + i + ("\n  "+i).join(
+            return "(TRY%s\n  " % props() + i + ("\n  "+i).join(
                     [o(n.tryBlock,i+"  ",c)] + [o(x,i+"  ",c)
                     for x in n.catchClauses]) + ")"
 
         elif n.type in ("VAR", "CONST"):
             check(subnodes=len(n))
-            return ("(%s%s " % (n.value, props())) + " ".join((o(x,i,c)
+            return ("(%s%s " % (n.value.upper(), props())) + " ".join((o(x,i,c)
                     for x in n)) + ")"
 
         elif n.type == "WITH":
             check(attrs=["body", "object"])
             sys.stderr.write("WARNING: A bad person wrote the code being "
                     "parsed. Don't use 'with'!\n")
-            return "(with%s %s %s)" % (props(), o(n.object,i,c), o(n.body,i,c))
+            return "(WITH%s %s %s)" % (props(), o(n.object,i,c), o(n.body,i,c))
 
         elif n.type == "WHILE":
             check(attrs=["condition","body","isLoop"])
             assert n.isLoop
-            return "(while%s %s\n  %s%s)" % (props(), o(n.condition,i,c), i,
+            return "(WHILE%s %s\n  %s%s)" % (props(), o(n.condition,i,c), i,
                     o(n.body, i+"  ",c))
 
         else:
